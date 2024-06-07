8e9ba8d4354db57cb5078150124bfc78bdf00d52
Allan <allanangelonf@gmail.com>
2024-06-07 15:01:16 -0300

    telacadastro e routes

--- projeto/ecommerce/src/App.jsx
+++ projeto/ecommerce/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Routes from './routes/Routes'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <Routes/>
     </>
   )
 }
---
+++ projeto/ecommerce/src/pages/TelaCadastro.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import api from '../api/api'
+import { useHistory } from 'react-router-dom'
+
+const Cadastro = () => {
+  const history = useHistory()
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post('/users', {
+        name,
+        email,
+       password
+      });
+      console.log('Usuário cadastrado com sucesso:', response.data);
+
+      setName('');
+      setEmail('');
+      setPassword('');
+
+      if (response.status === 201) {
+        history.push('/login');
+      }
+    } catch (error) {
+      console.error('Erro ao cadastrar usuário:', error);
+    }
+  };
+
+  return (
+    <>
+      <h2>Cadastro</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Nome:</label>
+        <br />
+        <input id="name" type="text" value={name} onChange={handleNameChange} />
+        <p />
+        <label htmlFor="email">Email:</label>
+        <br />
+        <input id="email" type="email" value={email} onChange={handleEmailChange} />
+        <p />
+        <label htmlFor="password">Senha:</label>
+        <br />
+        <input id="password" type="password" value={password} onChange={handlePasswordChange} />
+        <p />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </>
+  );
+};
+
+export default Cadastro;
